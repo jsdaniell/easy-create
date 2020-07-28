3ef5bbb59ab6d3643a95f444afd0f0b03f06a860
Daniel <jose.daniell@outlook.com>
2020-07-28 03:20:09 -0300

    Adjusted Routes

---
+++ src/components/generator/GeneratorView.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+export default function GeneratorView() {
+  return (
+    <Grid
+      container
+      style={{
+        padding: "25px 50px",
+        alignContent: "space-between",
+        minHeight: "100%"
+      }}
+      spacing={2}
+    >
+      dsads
+    </Grid>
+  );
+}
