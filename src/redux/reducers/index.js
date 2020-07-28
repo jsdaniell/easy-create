9155f3d77b8831bc3f0ce4f124b89371e851aec6
Daniel <jose.daniell@outlook.com>
2020-07-28 18:27:50 -0300

    Added Data Generator Module

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,21 +2,23 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import testCaseModalReducer from "./testCaseModalReducer";
-import userUidReducer from "./userUidReducer";
-import testListDocsReducer from "./testListDocsReducer";
-import testGroupsReducer from "./testGroupsReducer";
+import testCaseModalReducer from "./testCaseReducers/testCaseModalReducer";
+import userUidReducer from "./testCaseReducers/userUidReducer";
+import testListDocsReducer from "./testCaseReducers/testListDocsReducer";
+import testGroupsReducer from "./testCaseReducers/testGroupsReducer";
+import dataGeneratorReducer from "./dataGenerator/dataGeneratorReducer";
 
 const rootPersistConfig = {
   key: "root",
-  storage: storage,
+  storage: storage
 };
 
 const rootReducer = combineReducers({
   testCaseModalReducer,
   userUidReducer,
   testListDocsReducer,
-  testGroupsReducer
+  testGroupsReducer,
+  dataGeneratorReducer
 });
 
 export default persistReducer(rootPersistConfig, rootReducer);
