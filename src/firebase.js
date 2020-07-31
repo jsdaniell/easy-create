3bb587ea3257dd8e0a5467d42ddcf8787b68abfd
Daniel <jose.daniell@outlook.com>
2020-07-30 22:39:27 -0300

    Fixed Analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAPBexuJktp2lD3kinXzyT-7TOxL4eaaIg",
