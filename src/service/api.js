d6364414ceab3272090aab6c007b30bbb9daf9a5
Daniel <jose.daniell@outlook.com>
2020-08-15 05:44:16 -0300

    User Login Service Turned to API

--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,24 @@
-import axios from 'axios';
-const URL_API = 'https://sandbox.revobank.com.br/revobank/api';
+import axios from "axios";
+const URL_API = "https://dev-data-tools-api-golang.herokuapp.com";
 
 export const api = axios.create({
-  baseURL: URL_API,
+  baseURL: URL_API
 });
 
-api.interceptors.request.use(response => {
-  return response;
+// api.interceptors.request.use(
+//     config => {
+//       const token = sessionStorage.getItem('@ContaSoma-Token');
+//       if (token) {
+//         config.headers['Authorization'] = token;
+//       }
+//       return config;
+//     },
+//     error => {
+//       return Promise.reject(error);
+//     }
+// );
+
+api.interceptors.request.use(config => {
+  // config.headers["Content-Tyoe"] = "application/x-www-form-urlencoded";
+  return config;
 });
